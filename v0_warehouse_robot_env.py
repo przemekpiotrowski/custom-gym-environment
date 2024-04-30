d879811260576c922a7a92d3d0d12083856be5ad
Przemek Piotrowski <p@nic.com.pl>
2024-04-30 23:06:05 +0200

    wr gym environment

---
+++ v0_warehouse_robot_env.py
@@ -0,0 +1,79 @@
+import gymnasium as gym
+from gymnasium.envs.registration import register
+from gymnasium.utils.env_checker import check_env  # noqa
+from gymnasium import spaces
+
+import v0_warehouse_robot as wr
+import numpy as np
+
+register(
+    id="warehouse-robot-v0",
+    entry_point="v0_warehouse_robot_env:WarehouseRobotEnv",
+)
+
+
+class WarehouseRobotEnv(gym.Env):
+    metadata = {"render_modes": ["human"], "render_fps": 1}
+
+    def __init__(self, grid_rows=4, grid_cols=5, render_mode=None):
+        self.action_space = spaces.Discrete(len(wr.RobotAction))
+
+        self.grid_rows = grid_rows
+        self.grid_cols = grid_cols
+        self.observation_space = spaces.Box(
+            low=0,
+            high=np.array([self.grid_rows - 1, self.grid_cols - 1, self.grid_rows - 1, self.grid_cols - 1]),
+            shape=(4,),
+            dtype=np.int32,
+        )
+
+        self.wr = wr.WarehouseRobot(grid_rows=grid_rows, grid_cols=grid_cols)
+
+        self.render_mode = render_mode
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        self.wr.reset(seed=seed)
+
+        obs = np.concatenate((self.wr.robot_pos, self.wr.target_pos), dtype=np.int32)
+        info = {}
+
+        if self.render_mode == "human":
+            self.render()
+
+        return obs, info
+
+    def render(self):
+        self.wr.render()
+
+    def step(self, action):
+        target_reached = self.wr.perform_action(wr.RobotAction(action))
+
+        reward = 0
+        terminated = False
+        if target_reached:
+            reward = 1
+            terminated = True
+
+        obs = np.concatenate((self.wr.robot_pos, self.wr.target_pos), dtype=np.int32)
+        info = {}
+
+        if self.render_mode == "human":
+            print(wr.RobotAction(action))
+            self.render()
+
+        return obs, reward, terminated, False, info
+
+
+if __name__ == "__main__":
+    env = gym.make("warehouse-robot-v0", render_mode="human")
+    # check_env(env.unwrapped)
+
+    obs, info = env.reset()
+
+    while True:
+        rand_action = env.action_space.sample()
+        obs, reward, terminated, _, _ = env.step(rand_action)
+
+        if terminated:
+            obs, info = env.reset()
