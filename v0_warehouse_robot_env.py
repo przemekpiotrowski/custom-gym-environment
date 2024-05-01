a0be40b1dedd6c7724777af71d660e197daf2ce1
Przemek Piotrowski <p@nic.com.pl>
2024-05-02 00:08:52 +0200

    terminated vs truncated via the TimeLimit env wrapper

--- v0_warehouse_robot_env.py
+++ v0_warehouse_robot_env.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 from gymnasium.envs.registration import register
 from gymnasium.utils.env_checker import check_env  # noqa
 from gymnasium import spaces
+from gymnasium.wrappers import TimeLimit
 
 import v0_warehouse_robot as wr
 import numpy as np
@@ -67,13 +68,14 @@ class WarehouseRobotEnv(gym.Env):
 
 if __name__ == "__main__":
     env = gym.make("warehouse-robot-v0", render_mode="human")
+    env = TimeLimit(env, max_episode_steps=20)
+
     # check_env(env.unwrapped)
 
-    obs, info = env.reset()
-
-    while True:
+    env.reset()
+    terminated = truncated = False
+    while not terminated and not truncated:
         rand_action = env.action_space.sample()
-        obs, reward, terminated, _, _ = env.step(rand_action)
+        _, _, terminated, truncated, _ = env.step(rand_action)
 
-        if terminated:
-            obs, info = env.reset()
+    print(f"{terminated=}, {truncated=}")
