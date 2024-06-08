19cc4b374dd0d52ced15eef52cbe674ec5fb7db6
Przemek Piotrowski <p@nic.com.pl>
2024-06-08 22:46:03 +0200

    template env file

---
+++ _template_env.py
@@ -0,0 +1,48 @@
+import gymnasium as gym
+from gymnasium.envs.registration import register
+from gymnasium.utils.env_checker import check_env  # noqa
+from gymnasium import spaces
+
+import numpy as np
+
+
+register(
+    id="template-v0",
+    entry_point="_template_env:TemplateEnv",
+)
+
+
+class TemplateEnv(gym.Env):
+    metadata = {"render_modes": ["human"], "render_fps": 1}
+
+    def __init__(self, render_mode=None):
+        self.render_mode = render_mode
+
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(0, 1, shape=(2,), dtype=np.uint8)
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        obs = np.array([0, 1], dtype=np.uint8)
+        info = {}
+
+        return obs, info
+
+    def step(self, action):
+        obs = np.array([0, 1], dtype=np.uint8)
+        reward = 0
+        terminated = False
+        truncated = False
+        info = {}
+
+        return obs, reward, terminated, truncated, info
+
+    def render(self):
+        pass
+
+
+if __name__ == "__main__":
+    env = gym.make("template-v0", render_mode="human")
+
+    # check_env(env.unwrapped)
