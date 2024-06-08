c411dda148620fcd72116b9124cb0d6e2d9f3c86
Przemek Piotrowski <p@nic.com.pl>
2024-06-08 22:48:22 +0200

    template +TimeLimit

--- _template_env.py
+++ _template_env.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 from gymnasium.envs.registration import register
 from gymnasium.utils.env_checker import check_env  # noqa
 from gymnasium import spaces
+from gymnasium.wrappers import TimeLimit
 
 import numpy as np
 
@@ -44,5 +45,6 @@ class TemplateEnv(gym.Env):
 
 if __name__ == "__main__":
     env = gym.make("template-v0", render_mode="human")
+    env = TimeLimit(env, max_episode_steps=5)
 
-    # check_env(env.unwrapped)
+    check_env(env.unwrapped)
